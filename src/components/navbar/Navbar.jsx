c412fc989e3518191a54df07f691e4b9e3d980ac
chandan kumar <ck368966@gmail.com>
2025-02-01 03:34:04 +0530

    feat: add scroll effect to Navbar and update styles for fixed positioning

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,45 @@
-import React, { Fragment } from 'react'
-import { NavLink, useNavigate } from 'react-router-dom';
+import React, { Fragment, useEffect, useState } from 'react'
+import { Link, NavLink, useNavigate } from 'react-router-dom';
 import style from './Navbar.module.css'
 import { FaShopify } from "react-icons/fa";
 
 const Navbar = () => {
   const user_id = localStorage.getItem("userid");
   const navigate = useNavigate();
+  const [isScrolled, setIsScrolled] = useState(false);
+
   const logout = () => {
     navigate('/allproducts');
     localStorage.clear(user_id);
   }
-  
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 100) { // Change 100px as per your requirement
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <nav id={style.navbar}>
-      <figure><h1><FaShopify /> Fashion</h1></figure>
-      <ul>
-        <li><NavLink  to="/" className={({isActive})=> isActive? style.active : ""}>Home</NavLink></li>
+    <nav id={style.navbar} className={isScrolled ? style.fixed : ""}>
+      <figure>
+        <Link to="/">
+        <h1><FaShopify /> Fashion</h1>
+        </Link>
+        </figure>
+      <ul className={style.nav_item_container}>
+        <li><NavLink to="/" className={({ isActive }) => isActive ? style.active : ""}>Home</NavLink></li>
         {(user_id) ? (
-          <li><NavLink onClick={logout} className={({isActive})=> `${style.logout} ${isActive? style.active : ""}`}>Logout</NavLink></li>
+          <li><NavLink onClick={logout} className={({ isActive }) => `${style.logout} ${isActive ? style.active : ""}`}>Logout</NavLink></li>
         ) : (
           <Fragment>
-            <li><NavLink to="/login"  className={({isActive})=> `${style.login} ${isActive? style.active : "" }`}>Login</NavLink></li>
-            <li><NavLink to="/signup" className={({isActive})=> `${style.signup} ${isActive? style.active : ""}`}>SignUp</NavLink></li>
+            <li><NavLink to="/login" className={({ isActive }) => `${style.login} ${isActive ? style.active : ""}`}>Login</NavLink></li>
+            <li><NavLink to="/signup" className={({ isActive }) => `${style.signup} ${isActive ? style.active : ""}`}>SignUp</NavLink></li>
           </Fragment>
         )
         }
